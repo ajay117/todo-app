8caa58a492152c4074b9a1b0f05a6f77e0eb0a2e
Ajay KC <meajay64@gmail.com>
2022-09-21 23:18:15 +0900

    Install and configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'To do app',
+      template: './src/index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      //   {
+      //     test: /\.(woff|woff2|eot|ttf|otf)$/i,
+      //     type: 'asset/resource',
+      //   }
+    ],
+  },
+}
