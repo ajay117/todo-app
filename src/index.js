203ce788ae78d642a5356d9f2dd605eec2027480
Ajay KC <meajay64@gmail.com>
2022-09-24 00:36:01 +0900

    Add logic to add todo on form submit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
 import './styles.css'
+
+const form = document.querySelector('form')
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const input = document.querySelector('input')
+  const ul = document.querySelector('ul')
+  const div = document.createElement('div')
+  const li = document.createElement('li')
+  const span = document.createElement('span')
+  const para = document.createElement('p')
+  const img = document.createElement('img')
+
+  span.classList.add('circle')
+  para.textContent = input.value
+
+  div.appendChild(span)
+  div.appendChild(para)
+
+  img.setAttribute('src', '../images/icon-cross.svg')
+  img.setAttribute('alt', '')
+  img.classList.add('close-icon')
+
+  li.appendChild(div)
+  li.appendChild(img)
+
+  ul.appendChild(li)
+
+  input.value = '';
+})
