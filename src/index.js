d5120f5542edac3190950d83a4c145ca7fb91c35
Ajay KC <meajay64@gmail.com>
2022-09-25 13:36:38 +0900

    Remove unused code

--- src/index.js
+++ src/index.js
@@ -28,16 +28,6 @@ const elemObj = (() => {
   }
 })()
 
-// const form = document.querySelector('form')
-// const clearCompletedBtn = document.querySelector('.js-clear-completed')
-// const allToDo = document.querySelector('.all__todo')
-// const activeToDo = document.querySelector('.active__todo')
-// const completedToDo = document.querySelector('.completed__todo')
-// const darkTheme = document.querySelector('#theme__dark')
-// const lightTheme = document.querySelector('#theme__light')
-// const moonImage = document.querySelector('#theme__dark')
-// const sunImage = document.querySelector('#theme__light')
-
 elemObj.moonImage.setAttribute('src', moonIcon)
 elemObj.sunImage.setAttribute('src', sunIcon)
 
